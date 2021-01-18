7fbe97f9f3a279ecdb381d6a1db1f1e0b442adac
Adam O'Brien <17226864@studentmail.ul.ie>
2021-01-18 08:59:14 +0000

    Added husky git hooks

--- src/tests/Abilities.test.js
+++ src/tests/Abilities.test.js
@@ -65,7 +65,7 @@ describe('Permissions', () => {
     })
     test('cannot delete a topic which is not theirs', () => {
       // TODO: Implement check to see if they own the proposal or not
-      expect(ability.cannot('delete', topic({ publisher: 7 }))).toBeTruthy
+      expect(ability.cannot('delete', topic({ publisher: 7 }))).toBeFalsy
     })
   })
 })
